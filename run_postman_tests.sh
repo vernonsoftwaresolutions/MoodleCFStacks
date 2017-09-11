c982894c8ad3a8ec631e84c5fdd2618bd5e2c5f0
andrewlarsen <andrew.larsen@concordusa.com>
2017-09-10 22:10:35 -0500

    update to increase timeout

--- run_postman_tests.sh
+++ run_postman_tests.sh
@@ -2,4 +2,4 @@
 
 #Get output values, this is a somewhat naive approach since it is a lot of api calls
 
-newman run integration/MoodleStacks.postman_collection.json --timeout 5000
+newman run integration/MoodleStacks.postman_collection.json --timeout 15000
