2e7fde0473d0652ba8627073946aa183808a36bc
andrewlarsen <andrew.larsen@concordusa.com>
2017-10-09 21:29:30 -0500

    use --timeout-request

--- run_postman_tests.sh
+++ run_postman_tests.sh
@@ -2,4 +2,4 @@
 
 #Get output values, this is a somewhat naive approach since it is a lot of api calls
 
-newman run integration/MoodleStacks.postman_collection.json --timeout 15000
+newman run integration/MoodleStacks.postman_collection.json --timeout-request 15000
