846a21eb8ad320d044a4e13b184e219a2344d7da
andrewlarsen <andrew.larsen@concordusa.com>
2017-10-20 21:28:45 -0500

    fix deployment

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,8 @@ VERSION=$2
 ENV=$3
 DNSNAME=$4
 HOSTEDZONENAME=$5
+HANDLER_1=$6
+HANDLER_2=$7
 
 API_NAME="GetCFStack"
 
@@ -44,11 +46,11 @@ echo "deploymentId ${deploymentId}"
 ##
 # Get the lambda function names and store them as a file
 ##
-aws cloudformation describe-stacks --stack-name "${API_NAME}" \
-    --query "Stacks[0].[Outputs[?starts_with(OutputValue, 'arn:aws:lambda')]][0][*].{OutputValue:OutputValue}" \
-    --output text > names
-
-echo "retrieved lambda functions, now publish versions"
+#aws cloudformation describe-stacks --stack-name "${API_NAME}" \
+#    --query "Stacks[0].[Outputs[?starts_with(OutputValue, 'arn:aws:lambda')]][0][*].{OutputValue:OutputValue}" \
+#    --output text > names
+#
+#echo "retrieved lambda functions, now publish versions"
 ##
 # Now read each lambda function and publish a new version
 ##
@@ -61,6 +63,26 @@ while read name; do
     echo "Version ${LAMBDAVERSION} published"
 done < names
 
+HANDLER_1_NAME=`aws cloudformation describe-stacks --stack-name  "${API_NAME}" \
+  --query "Stacks[0].[Outputs[?ends_with(OutputValue, '"${HANDLER_1}"')]][0][*].{OutputValue:OutputValue}"`
+
+
+echo "retrieved lambda function ${HANDLER_1_NAME}"
+
+HANDLER_2_NAME=`aws cloudformation describe-stacks --stack-name  "${API_NAME}" \
+  --query "Stacks[0].[Outputs[?ends_with(OutputValue, '"${HANDLER_2}"')]][0][*].{OutputValue:OutputValue}"`
+
+echo "retrieved lambda function ${HANDLER_2_NAME}"
+
+LAMBDAVERSION_1=`aws lambda publish-version --function-name ${HANDLER_1_NAME} \
+               --description ${VERSION} \
+               --query '{Version:Version}' \
+               --output text`
+
+LAMBDAVERSION_2=`aws lambda publish-version --function-name ${HANDLER_2_NAME} \
+                --description ${VERSION} \
+                --query '{Version:Version}' \
+                --output text`
 ##
 # Publish new lambda version
 ##
@@ -84,12 +106,13 @@ aws cloudformation package --template-file \
 ##
 # Deploy template
 ##
-echo "about to deploy environment with variables ${apiGatewayApiRef} ${ENV} ${deploymentId} ${VERSION} ${DNSNAME}"
+echo "about to deploy environment with variables ${apiGatewayApiRef} ${ENV} ${deploymentId} ${LAMBDAVERSION_1} ${LAMBDAVERSION_2} ${DNSNAME}"
 
 aws cloudformation deploy --template-file \
     formation_env_output.yaml --capabilities CAPABILITY_IAM \
     --stack-name "${API_NAME}-${ENV}" \
     --parameter-overrides ApiGateway="${apiGatewayApiRef}" \
-    StageName="${ENV}" DeploymentId="${deploymentId}" DomainName="${DNSNAME}" Version="${VERSION}" DomainName="${DNSNAME}"|| exit 0
+     StageName="${ENV}" DeploymentId="${deploymentId}" DomainName="${DNSNAME}" \
+     GetVersion="${LAMBDAVERSION_1}" DeleteVersion="${LAMBDAVERSION_2}" DomainName="${DNSNAME}"|| exit 0
 
 
